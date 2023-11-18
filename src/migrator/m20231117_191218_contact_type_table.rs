ec4681ccd2280250ab26cbfafc034761bf39688b
bimalkeeth <bimalkeeth@gmail.com>
2023-11-18 20:26:46 +1100

    implementing migrations

--- src/migrator/m20231117_191218_contact_type_table.rs
+++ src/migrator/m20231117_191218_contact_type_table.rs
@@ -9,29 +9,29 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(CantactType::Table)
+                    .table(ContactType::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(CantactType::Id)
+                        ColumnDef::new(ContactType::Id)
                             .integer()
                             .not_null()
                             .generated("uuid-ossp",true)
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(CantactType::Name).string().not_null())
-                    .col(ColumnDef::new(CantactType::Description).string().null())
+                    .col(ColumnDef::new(ContactType::Name).string().not_null())
+                    .col(ColumnDef::new(ContactType::Description).string().null())
                     .to_owned(),
             )
             .await
     }
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(CantactType::Table).to_owned())
+            .drop_table(Table::drop().table(ContactType::Table).to_owned())
             .await
     }
 }
 #[derive(DeriveIden)]
-enum CantactType {
+enum ContactType {
     Table,
     Id,
     Name,
