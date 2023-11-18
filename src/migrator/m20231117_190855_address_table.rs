6ab23ac5da23be23a7f0d1a2807084d567fdfbc0
bimalkeeth <bimalkeeth@gmail.com>
2023-11-18 21:04:53 +1100

    implementing migrations

--- src/migrator/m20231117_190855_address_table.rs
+++ src/migrator/m20231117_190855_address_table.rs
@@ -1,5 +1,7 @@
 use sea_orm_migration::prelude::*;
 use crate::migrator::BinOper::Add;
+use crate::migrator::m20220101_000001_staff_table::Staff;
+
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -24,13 +26,17 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Address::State).string().not_null())
                     .col(ColumnDef::new(Address::PostCode).string().not_null())
                     .col(ColumnDef::new(Address::Country).string().not_null())
+                    .col(ColumnDef::new(Address::StaffID).uuid().not_null())
+                    .foreign_key(ForeignKey::create().name("fk-staff-address").
+                        from(Address::Table,Address::StaffID).
+                        to(Staff::Table,Staff::Id))
                     .to_owned(),
             )
             .await
     }
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Address::Table).to_owned())
             .await
     }
 }
@@ -39,6 +45,7 @@ impl MigrationTrait for Migration {
 pub enum Address {
     Table,
     Id,
+    StaffID,
     Street,
     Suburb,
     State,
