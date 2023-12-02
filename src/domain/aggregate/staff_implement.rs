7567a5626b88c6467d2905f32a7161c3f22968ec
bimalkeeth <bimalkeeth@gmail.com>
2023-12-02 21:28:55 +1100

    implementing domain aggregates

--- src/domain/aggregate/staff_implement.rs
+++ src/domain/aggregate/staff_implement.rs
@@ -5,95 +5,92 @@ use crate::domain::common::EntityType;
 use std::any::Any;
 
 impl AggStaff {
-    pub fn get_staff_event(&self, cmd_staff: Box<Option<CmdStaff>>) -> Option<&mut EventStaff> {
-        let staff = self.
-            compose_staff(cmd_staff, true);
+    pub fn get_staff_event(&self, cmd_staff: Box<Option<CmdStaff>>) -> Option<EventStaff> {
+        let staff = self.compose_staff(cmd_staff, true);
 
         return staff;
     }
 
-    pub fn get_address_event(&self, cmd_address: Box<Option<AggAddress>>) -> Option<& mut EventStaff> {
-        let staff = self.
-            compose_staff(Box::new(None), true);
+    pub fn get_address_event(&self, cmd_address: Box<Option<AggAddress>>) -> Option<EventStaff> {
+        let staff = self.compose_staff(Box::new(None), true);
 
         let address = self.compose_address(cmd_address);
-        if let Some(staff) = staff {
-            staff.address = address
+
+        if let Some(mut staff_opt) = &staff {
+            staff_opt.address = address;
+
+            return Some(staff_opt);
         }
 
         return staff;
     }
 
-    pub fn get_contact_event(&self, cmd_contact: Box<Option<AggContact>>) -> Option<&mut EventStaff> {
-        let staff = self.
-            compose_staff(Box::new(None), true);
+    pub fn get_contact_event(&self, cmd_contact: Box<Option<AggContact>>) -> Option<EventStaff> {
+        let staff = self.compose_staff(Box::new(None), true);
 
         let contacts = self.compose_contact(cmd_contact);
-        if let Some(staff) = staff {
-            staff.contacts = contacts
+        if let Some(mut staff_opt) = &staff {
+            staff_opt.contacts = contacts;
+
+            return Some(staff_opt);
         }
 
         return staff;
     }
 
-    pub(crate) fn compose_staff(&self, opt_staff: Box<Option<CmdStaff>>, staff_only: bool) -> Option<&mut EventStaff> {
+    pub(crate) fn get_staff(&self, staff: &dyn StaffIn, staff_only: bool) -> Option<EventStaff> {
+        let address = |staff_only: bool| -> Vec<EventAddress>{
+            if staff_only {
+                return self.compose_address(Box::new(None));
+            }
+
+            return Vec::<EventAddress>::new();
+        };
+
+        let contact = |staff_only: bool| -> Vec<EventContact>{
+            if staff_only {
+                return self.compose_contact(Box::new(None));
+            }
+
+            return Vec::<EventContact>::new();
+        };
+
+
+        let staff_upd: EventStaff = EventStaff {
+            id: staff.id().to_string(),
+            first_name: staff.first_name().to_string(),
+            last_name: staff.last_name().to_string(),
+            vehicle_reg: staff.vehicle_reg().to_string(),
+            driver_license: staff.driver_license().to_string(),
+            in_contract: staff.in_contract(),
+            active: staff.active(),
+            address: address(staff_only),
+            contacts: contact(staff_only),
+        };
+
+        return Some(staff_upd);
+    }
+
+    pub(crate) fn compose_staff(&self, opt_staff: Box<Option<CmdStaff>>, staff_only: bool) -> Option<EventStaff> {
         let optional_staff = *opt_staff;
 
-        let staff_fn = |staff: CmdStaff, staff_only: bool| -> &mut EventStaff {
-            let mut staff_upd = EventStaff {
-                id: staff.id.to_string(),
-                first_name: staff.first_name.to_string(),
-                last_name: staff.last_name.to_string(),
-                vehicle_reg: staff.vehicle_reg.to_string(),
-                driver_license: staff.driver_license.to_string(),
-                in_contract: staff.in_contract,
-                active: false,
-                address: vec![],
-                contacts: vec![],
-            };
-
-            if staff_only {
-                staff_upd.address = self.compose_address(Box::new(None));
-                staff_upd.contacts = self.compose_contact(Box::new(None));
-            }
-            return &mut staff_upd;
-        };
-
-        let event_fn = |staff: &AggStaff, staff_only: bool| -> &mut EventStaff{
-            let mut staff_upd = EventStaff {
-                id: staff.id.to_string(),
-                first_name: staff.first_name.to_string(),
-                last_name: staff.last_name.to_string(),
-                vehicle_reg: staff.vehicle_reg.to_string(),
-                driver_license: staff.driver_license.to_string(),
-                in_contract: staff.in_contract,
-                active: false,
-                address: vec![],
-                contacts: vec![],
-            };
-
-            if staff_only {
-                staff_upd.address = self.compose_address(Box::new(None));
-                staff_upd.contacts = self.compose_contact(Box::new(None));
-            }
-
-            return &mut staff_upd;
-        };
-
-
-        return match optional_staff {
+        let stf = return match optional_staff {
             Some(staff) => {
-                let stf = staff_fn(staff, staff_only);
-                Some(stf)
+                let result_staff = self.get_staff(&staff, staff_only);
+
+                result_staff
             }
             None => {
-                let stf = event_fn(self, staff_only);
-                Some(stf)
+                let result_staff = self.get_staff(self, staff_only);
+
+                result_staff
             }
         };
     }
+
     pub(crate) fn compose_address(&self, opt_add: Box<Option<AggAddress>>) -> Vec<EventAddress> {
         let mut address: Vec<EventAddress> = Vec::new();
+
         let address_func = |add_vec: &mut Vec<EventAddress>, addr: &AggAddress| {
             add_vec.push(EventAddress {
                 id: addr.id.to_string(),
@@ -118,7 +115,6 @@ impl AggStaff {
         }
         return address;
     }
-
     pub(crate) fn compose_contact(&self, opt_con: Box<Option<AggContact>>) -> Vec<EventContact> {
         let mut contacts: Vec<EventContact> = Vec::new();
         let contact_func = |con_vec: &mut Vec<EventContact>, con: &AggContact| {
