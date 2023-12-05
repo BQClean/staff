1fab891702366eec7ce1090eb6d6f3730ee6a420
bimalkeeth <bimalkeeth@gmail.com>
2023-12-06 05:11:12 +1100

    implementing domain aggregates

--- src/services/svc_staff.rs
+++ src/services/svc_staff.rs
@@ -19,16 +19,28 @@ impl IStaffService for StaffService {
         let staff= command.as_ref();
         match staff {
             Some(ref stf)=>{
-                if stf.id == ""{
-                    
+                if stf.id.is_empty(){
+                    return Err(StaffError("id is not defined".to_string()))
+                }
+
+                if stf.first_name.is_empty(){
+                    return Err(StaffError("first name is not defined".to_string()))
+                }
+
+                if stf.last_name.is_empty(){
+                    return Err(StaffError("last name is not defined".to_string()))
+                }
+
+                if stf.driver_license.is_empty(){
+                    return Err(StaffError("driver licence is not defined".to_string()))
                 }
             }
             None=>{
-
+               return Err(StaffError("staff is not defined".to_string()))
             }
         }
 
-        todo!()
+       Ok(())
     }
 
     async fn validate_address(&self, command: Box<Option<CmdAddress>>) -> Result<(), StaffError> {
