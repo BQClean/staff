80edf63cfda885df30e4d7ab4c50891b342a0014
bimalkeeth <bimalkeeth@gmail.com>
2023-12-24 16:05:15 +1100

    implement event update

--- src/domain/aggregate/event_data.rs
+++ src/domain/aggregate/event_data.rs
@@ -27,7 +27,6 @@ impl AggStaff {
         &mut self,
         event: &CommonEvent<EventStaff>) {
         for add in event.data.address.iter() {
-
             let ops = |ad:&EventAddress| -> Operation {
                 match ad.operation {
                     Operation::On =>  Operation::On,
@@ -53,10 +52,59 @@ impl AggStaff {
     pub(crate) fn process_address_updated_event(
         &mut self,
         event: &CommonEvent<EventStaff>) {
+        let ops = |ad:&EventAddress| -> Operation {
+            match ad.operation {
+                Operation::On =>  Operation::On,
+                Operation::Off => Operation::Off,
+            }
+        };
 
+        for add in event.data.address.iter(){
+           let address = self.address.iter_mut().find(|address:&&mut AggAddress|->bool{
+                address.id==add.id
+            });
+
+            match address {
+                Some(a) => {
+                    a.primary=add.primary;
+                    a.country=add.country.to_string();
+                    a.staff_id=add.staff_id.to_string();
+                    a.state=add.state.to_string();
+                    a.post_code=add.post_code.to_string();
+                    a.operation=ops(add);
+                    a.street=add.street.to_string();
+                }
+                None=> {
+                    continue
+                }
+            }
+        }
     }
 
-    pub(crate) fn process_contact_created_event(&mut self, event: &CommonEvent<EventStaff>) {}
+    pub(crate) fn process_contact_created_event(
+        &mut self,
+        event: &CommonEvent<EventStaff>) {
+        let ops = |cn:&EventContact| -> Operation {
+            match cn.operation {
+                Operation::On =>  Operation::On,
+                Operation::Off => Operation::Off,
+            }
+        };
+
+        for con in event.data.contacts.iter(){
+            let contact = AggContact{
+                id:con.id.to_string(),
+                contact_type_id:con.contact_type_id.to_string(),
+                contact_value:con.contact_value.to_string(),
+                primary:con.primary,
+                staff_id:con.staff_id.to_string(),
+                operation:ops(con)
+            };
+
+            self.contacts.push(contact);
+        }
+
+    }
 
     pub(crate) fn process_contact_updated_event(&mut self, event: &CommonEvent<EventStaff>) {}
 }
