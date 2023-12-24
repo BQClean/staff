a1b3db774566ad6f50e185449c7da49ad4d295d2
bimalkeeth <bimalkeeth@gmail.com>
2023-12-24 17:43:19 +1100

    implement event update

--- src/domain/aggregate/event_data.rs
+++ src/domain/aggregate/event_data.rs
@@ -27,14 +27,14 @@ impl AggStaff {
         &mut self,
         event: &CommonEvent<EventStaff>) {
         for add in event.data.address.iter() {
-            let ops = |ad:&EventAddress| -> Operation {
+            let ops = |ad: &EventAddress| -> Operation {
                 match ad.operation {
-                    Operation::On =>  Operation::On,
+                    Operation::On => Operation::On,
                     Operation::Off => Operation::Off,
                 }
             };
 
-            let address=AggAddress{
+            let address = AggAddress {
                 id: add.id.to_string(),
                 street: add.street.to_string(),
                 state: add.state.to_string(),
@@ -42,7 +42,7 @@ impl AggStaff {
                 country: add.country.to_string(),
                 staff_id: add.staff_id.to_string(),
                 primary: add.primary,
-                operation:ops(add)
+                operation: ops(add),
             };
 
             self.address.push(address)
@@ -52,30 +52,30 @@ impl AggStaff {
     pub(crate) fn process_address_updated_event(
         &mut self,
         event: &CommonEvent<EventStaff>) {
-        let ops = |ad:&EventAddress| -> Operation {
+        let ops = |ad: &EventAddress| -> Operation {
             match ad.operation {
-                Operation::On =>  Operation::On,
+                Operation::On => Operation::On,
                 Operation::Off => Operation::Off,
             }
         };
 
-        for add in event.data.address.iter(){
-           let address = self.address.iter_mut().find(|address:&&mut AggAddress|->bool{
-                address.id==add.id
+        for add in event.data.address.iter() {
+            let address = self.address.iter_mut().find(|address: &&mut AggAddress| -> bool{
+                address.id == add.id
             });
 
             match address {
                 Some(a) => {
-                    a.primary=add.primary;
-                    a.country=add.country.to_string();
-                    a.staff_id=add.staff_id.to_string();
-                    a.state=add.state.to_string();
-                    a.post_code=add.post_code.to_string();
-                    a.operation=ops(add);
-                    a.street=add.street.to_string();
+                    a.primary = add.primary;
+                    a.country = add.country.to_string();
+                    a.staff_id = add.staff_id.to_string();
+                    a.state = add.state.to_string();
+                    a.post_code = add.post_code.to_string();
+                    a.operation = ops(add);
+                    a.street = add.street.to_string();
                 }
-                None=> {
-                    continue
+                None => {
+                    continue;
                 }
             }
         }
@@ -84,27 +84,50 @@ impl AggStaff {
     pub(crate) fn process_contact_created_event(
         &mut self,
         event: &CommonEvent<EventStaff>) {
-        let ops = |cn:&EventContact| -> Operation {
+        let ops = |cn: &EventContact| -> Operation {
             match cn.operation {
-                Operation::On =>  Operation::On,
+                Operation::On => Operation::On,
                 Operation::Off => Operation::Off,
             }
         };
 
-        for con in event.data.contacts.iter(){
-            let contact = AggContact{
-                id:con.id.to_string(),
-                contact_type_id:con.contact_type_id.to_string(),
-                contact_value:con.contact_value.to_string(),
-                primary:con.primary,
-                staff_id:con.staff_id.to_string(),
-                operation:ops(con)
+        for con in event.data.contacts.iter() {
+            let contact = AggContact {
+                id: con.id.to_string(),
+                contact_type_id: con.contact_type_id.to_string(),
+                contact_value: con.contact_value.to_string(),
+                primary: con.primary,
+                staff_id: con.staff_id.to_string(),
+                operation: ops(con),
             };
 
             self.contacts.push(contact);
         }
-
     }
 
-    pub(crate) fn process_contact_updated_event(&mut self, event: &CommonEvent<EventStaff>) {}
+    pub(crate) fn process_contact_updated_event(
+        &mut self,
+        event: &CommonEvent<EventStaff>) {
+        let ops = |cn: &EventContact| -> Operation {
+            match cn.operation {
+                Operation::On => Operation::On,
+                Operation::Off => Operation::Off,
+            }
+        };
+        for con in event.data.contacts.iter() {
+            let contact = self.contacts.iter_mut().find(|co: &&mut AggContact| -> bool{
+                co.id == con.id
+            });
+
+            match contact {
+                Some(cn) => {
+                    cn.primary=con.primary;
+                    cn.contact_value=con.contact_value.to_string();
+                    cn.contact_type_id=con.contact_type_id.to_string();
+                    cn.staff_id=con.staff_id.to_string();
+                    cn.operation=ops(con);
+                }
+            }
+        }
+    }
 }
