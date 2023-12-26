1ac44cbbbe8a80df9ceb7d8de7a44da4b27d1e5b
bimalkeeth <bimalkeeth@gmail.com>
2023-12-26 22:41:17 +1100

    implement entity library

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@
 
 use sea_orm_migration::MigratorTrait;
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::spawn;
 use tokio::sync::oneshot;
 use tokio::sync::oneshot::{Receiver, Sender};
+use tonic::transport::Server;
 use crate::config::config::Config;
 use config::trait_config::IConfig;
 use crate::infrastructure::repository::connect::data_connection::DBConnection;
 use infrastructure::repository::connect::trait_connection::TConnection;
+use crate::application::api::{StaffServiceApi};
+use crate::pbstaff::staff_server_service_server::StaffServerServiceServer;
 
 mod adapters;
 mod application;
@@ -40,6 +44,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     let service_address = cfg.get_service_address();
+    let api_server = StaffServiceApi::new();
+
+    let(signal_tx,signal_rx) =signal_channel();
+    spawn(wait_for_sigterm(signal_tx));
+
+    Server::builder().add_service(
+        StaffServerServiceServer::new(api_server))
+        .serve_with_shutdown(service_address.parse()?,async {
+       signal_rx.await.ok();
+    }).await?;
 
    Ok(())
 }
@@ -51,8 +65,6 @@ async fn wait_for_sigterm(tx:Sender<()>){
         .expect("failed to install signal handler")
         .recv().await;
 
-
     println!("SIGTERM received : shutting down");
-
     let _ = tx.send(());
 }
